3d1e9f8e584cb779d8a80b89f6d2bc0a42138db2
Dan Spencer <dspence180@gmail.com>
2020-05-28 00:37:10 -0700

    Tutorial 2 End

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,33 @@
 # Flask Blog app
 # Env Variables: FLASK_APP=flaskblog.py, FLASK_DEBUG=1
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+posts = [
+    {
+        'author': 'Dan Spencer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 28, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 29, 2020'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
